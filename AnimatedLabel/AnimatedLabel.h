b8fffc724b5120113444ed7e1d703a51f14bc56a
Ben Gottlieb <ben@standalone.com>
2017-02-02 13:12:01 -0600

    added Library target

---
+++ AnimatedLabel/AnimatedLabel.h
@@ -0,0 +1,30 @@
+//
+//  AnimatedLabel.h
+//  AnimatedLabel
+//
+//  Created by Ben Gottlieb on 2/2/17.
+//  Copyright © 2017 alipay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimatedLabel.
+FOUNDATION_EXPORT double AnimatedLabelVersionNumber;
+
+//! Project version string for AnimatedLabel.
+FOUNDATION_EXPORT const unsigned char AnimatedLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimatedLabel/PublicHeader.h>
+
+
+#import "ZCAnimatedLabel.h"
+#import "ZCFallLabel.h"
+#import "ZCDashLabel.h"
+#import "ZCDuangLabel.h"
+#import "ZCFlyinLabel.h"
+#import "ZCFocusLabel.h"
+#import "ZCRevealLabel.h"
+#import "ZCShapeshiftLabel.h"
+#import "ZCSpinLabel.h"
+#import "ZCThrownLabel.h"
+#import "ZCTransparencyLabel.h"
